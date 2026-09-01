dc399c2b1d432a2ebf47befea4e42e1140e11da9
Theprogrammingstudent <64977945+Theprogrammingstudent@users.noreply.github.com>
2026-09-01 22:49:13 +0100

    Add: find-target-in-rotated-sorted-array - submission-6

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-6.py
@@ -0,0 +1,23 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l,r = 0,len(nums)-1
+
+        while l <= r:
+            m = (l+r)//2
+
+            if nums[m] == target:
+                return m
+            
+            if nums[l] <= nums[m]:
+                if target > nums[m] or target < nums[l]:
+                    l += 1
+                else:
+                    r -= 1
+            
+            else:
+                if target < nums[m] or target > nums[r]:
+                    r -= 1
+                else:
+                    l += 1
+
+        return -1
